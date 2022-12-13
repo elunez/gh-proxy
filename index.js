f1b0b919ac999c506a142250e0b6951f09fc297d
elunez <42142420+elunez@users.noreply.github.com>
2022-12-13 11:31:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ function httpHandler(req, pathname) {
     if (!flag) {
         return new Response("blocked", {status: 403})
     }
-    if (urlStr.startsWith('github')) {
+    if (urlStr.startsWith('github') || urlStr.startsWith('raw') || urlStr.startsWith('gist')) {
         urlStr = 'https://' + urlStr
     }
     const urlObj = newUrl(urlStr)
